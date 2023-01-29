056862aa42c6e375aab5499a531678117a51b064
HamInGyu <hfire1207@gmail.com>
2023-01-29 17:50:18 +0900

    in9yuBlog nextApp initialSetting

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main>안농안농</main>
+    </>
+  );
+}
