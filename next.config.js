b9e0a4e99998af161656009e547213778016b64e
HamInGyu <hfire1207@gmail.com>
2023-01-31 23:24:51 +0900

    [INSTALL] Pretendard 폰트 셋팅

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
-  },
 };
 
 module.exports = nextConfig;
